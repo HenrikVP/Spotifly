9b1351219f3e7e53ebb9e3c34feeea8cd2acd6ab
HenrikVP <hpo@tec.dk>
2023-12-07 13:01:47 +0100

    Add project files.

---
+++ Spotifly/Data.cs
@@ -0,0 +1,54 @@
+﻿namespace Spotifly
+{
+    internal class Data
+    {
+        public static List<Song> Songs { get; set; }// = new();
+        public static List<Artist> Artists { get; set; } = new();
+        public static List<Playlist> Playlists { get; set; } = new();
+
+        public static void AddData()
+        {
+            Artist a1 = new Artist();
+            a1.Name = "Michael Jackson";
+            a1.Birthday = new DateTime(1958, 8, 29);
+            a1.User = "Henrik";
+            a1.Country = "USA";
+            Artists.Add(a1);
+
+            Artist a2 = new()
+            {
+                Name = "Bob Marley",
+                Country = "Jamaica",
+                Birthday = new DateTime(1945, 2, 6)
+            };
+            Artists.Add(a2);
+
+            var s1 = new Song()
+            {
+                Title = "Stay with me",
+                Genre = "Pip Roeggae",
+                Length = 225,
+                ReleaseDate = new DateTime(1984, 12, 1),
+                Artists = new List<Artist>() { a1, a2 },
+                User = "Fans"
+            };
+
+            var s2 = new Song()
+            {
+                Title = "Stay with me part 2",
+                Genre = "Pip Roeggae",
+                Length = 225,
+                ReleaseDate = new DateTime(1985, 12, 1),
+                Artists = new List<Artist>() { a1, a2 },
+                User = "Fans"
+            };
+
+            Songs = new List<Song>();
+            Songs.AddRange(new List<Song>(){ s1, s2});
+
+            Playlist p1 = new("Bedroom", new List<Song>() { s1, s2 });
+
+            Playlists.Add(p1);
+        }
+    }
+}
